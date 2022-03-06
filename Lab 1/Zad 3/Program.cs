f32284ca4fa1e96ce881a477991720aa5c82992c
emch43l <michalgood10@gmail.com>
2022-03-06 16:33:04 +0100

    zad 3 complete

--- Lab 1/Zad 3/Program.cs
+++ Lab 1/Zad 3/Program.cs
@@ -6,7 +6,32 @@ namespace Zad_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] tab = { 1, 2, 1, 2, 5, 10, 1, 2, 3, 4, 5, 6 };
+            int k = 6;
+            Console.WriteLine(SearchNumberInIncreasing(tab, k));
         }
+
+        static int SearchNumberInIncreasing(int[] tab, int k)
+        {
+            int? prev = null;
+            int iterator = 0;
+            for(int i = 0; i < tab.Length; i++)
+            {
+                if(tab[i] > prev || prev == null)
+                {
+                    iterator++;
+                    if (iterator == k)
+                        return tab[i];
+                }
+                else
+                {
+                    iterator = 1;
+                }
+                prev = tab[i];
+            }
+
+            return -1;
+        }
+
     }
 }
