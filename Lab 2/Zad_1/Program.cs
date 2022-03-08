7f7c2231a27501700e57eea25ef6f1cffdf39fa6
michal.mierzwa <michal.mierzwa@PK5-16>
2022-03-08 09:18:57 +0100

    added lab 2

---
+++ Lab 2/Zad_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Zad_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tab = { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine(sumTab(tab));
+        }
+
+        static int sumTab(int[] tab, int sum = 0, int index = 0)
+        {
+            if (index == tab.Length)
+                return sum;
+
+            return sumTab(tab, sum += tab[index],index+1);
+        }
+    }
+}
