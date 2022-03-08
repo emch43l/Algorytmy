72b296c0e559c4a989ec76ab092ffd779d17b898
michal.mierzwa <michal.mierzwa@PK5-16>
2022-03-08 10:38:47 +0100

    Zad 4 Lab 2 w toku

---
+++ Lab 2/Zad_4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Zad_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Element(3,2));
+        }
+
+        static float Element(int S, int n, float prevElement = 0, int count = 0)
+        {
+            float Xn;
+
+            if (count == 0)
+                Xn = (1 / 2) * ((S / 2) + S / (S / 2));
+            else
+                Xn = (float)(1 / 2) * (float)(prevElement + (float)((float)S / prevElement));
+
+            if (count == n)
+                return Xn;
+
+            Console.WriteLine(Xn);
+
+            count++;
+            return Element(S, n, Xn, count + 1);
+
+        }
+    }
+}
