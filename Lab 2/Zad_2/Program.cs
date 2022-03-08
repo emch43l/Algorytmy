a9b8eabe33e468713c64092b9f20d5006dab7004
michal.mierzwa <michal.mierzwa@PK5-16>
2022-03-08 09:26:21 +0100

    zad 2 lab 2 complete

---
+++ Lab 2/Zad_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Zad_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tab = { 1, 2, 3, 4, 5, 2 };
+            int element = 6;
+            Console.WriteLine(countElement(tab, element));
+        }
+
+        static int countElement(int[] tab, int element, int index = 0, int elementCount = 0)
+        {
+
+            if (index == tab.Length)
+                return elementCount;
+
+            if (element == tab[index])
+                elementCount++;
+
+            return countElement(tab, element, index + 1, elementCount);
+        }
+    }
+}
