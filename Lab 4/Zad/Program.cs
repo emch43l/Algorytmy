1e3455f428487cd3c2671d754bb9e0c14f4cb480
michal.mierzwa <michal.mierzwa@PK5-16>
2022-03-22 10:37:57 +0100

    adasd

---
+++ Lab 4/Zad/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Zad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tab = { 9, 4, 8, 2, 5, 6, 7, 3, 1 };
+            sortTab(tab);
+        }
+
+        static int sortTab(int[] tab)
+        {
+            int? min = tab[0];
+
+            for(int i = 0; i < tab.Length; i++)
+            {
+                for(int j = 0 + i; j < tab.Length; j++)
+                {
+                    if(min > tab[j])
+                    {
+                        
+                    }
+                }
+            }
+
+            return 0;
+        }
+
+    }
+}
