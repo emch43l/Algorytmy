f1f40948a75c349557eeb10526309b95c986a930
michal.mierzwa <michal.mierzwa@PK5-16>
2022-04-12 09:13:40 +0200

    Lab 7

---
+++ Lab 7/Zad/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Zad
+{
+    class Program
+    {
+        class Node<T>
+        {
+            public T Value { get; set; }
+            public Node<T> Next { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            Node<string> node = new Node<string> { Value = "adam" };
+            node.Next = new Node<string> { Value = "ewa" };
+            node.Next.Next = new Node<string> { Value = "karol" };
+            Node<string> head = node;
+            while(node != null)
+            {
+                Console.WriteLine(node.Value);
+                node = node.Next;
+            }
+            
+        }
+    }
+}
