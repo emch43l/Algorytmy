f700871c393b650f917267e877410ca24d349948
michal.mierzwa <michal.mierzwa@PK5-16>
2022-04-12 10:31:58 +0200

    Zad 3, Zad 4 Complete

--- Lab 7/Zad/Program.cs
+++ Lab 7/Zad/Program.cs
@@ -29,12 +29,22 @@ namespace Zad
                 _head = _head.Next;
                 return result;
             }
+            // zad 3
+            public Stack<T> Reverse()
+            {
+                Stack<T> oldStack = this;
+                Stack<T> newStack = new Stack<T>();
+                while(!oldStack.isEmpty())
+                    newStack.Push(oldStack.Pop());
+                return newStack;
+            }
         }
 
         class Queue<T>
         {
             private Node<T> _head;
             private Node<T> _tail;
+            private int _count;
             public bool IsEmpty() => _head == null;
             public void Insert(T value)
             {
@@ -43,8 +53,10 @@ namespace Zad
                 {
                     _head = node;
                     _tail = _head;
+                    _count++;
                     return;
                 }
+                _count++;
                 _tail.Next = node;
                 _tail = node;
             }
@@ -54,7 +66,27 @@ namespace Zad
                     throw new Exception("queue is empty");
                 T result = _head.Value;
                 _head = _head.Next;
+                _count--;
                 return result;
+            }
+
+            public T[] ToArray()
+            {
+                if (IsEmpty())
+                    return new T[0];
+                T[] array = new T[_count];
+                Node<T> node = _head;
+                int iterator = 0;
+                while(node != null)
+                {
+                    array[iterator++] = node.Value;
+                    // ^^^ to samo co
+                    // array[iterator] = node.Value
+                    // i++
+                    node = node.Next;
+                }
+
+                return array;
             }
         }
 
@@ -74,16 +106,53 @@ namespace Zad
             stack.Push("Adam");
             stack.Push("Ewa");
             stack.Push("Karol");
+            stack = stack.Reverse();
             while (!stack.isEmpty())
                 Console.WriteLine(stack.Pop());
 
-            Queue<string> queue = new Queue<string>();
-            queue.Insert("Adam");
-            queue.Insert("Ewa");
-            queue.Insert("Karol");
-            while (!queue.IsEmpty())
-                Console.WriteLine(queue.Remove());
+            string brackets = "((()))[](([]))(([{(())}]))";
 
+            CheckBrackets check = new CheckBrackets(brackets);
+            Console.WriteLine(check.validate());
+
+            //Queue<string> queue = new Queue<string>();
+            //queue.Insert("Adam");
+            //queue.Insert("Ewa");
+            //queue.Insert("Karol");
+            //while (!queue.IsEmpty())
+            //    Console.WriteLine(queue.Remove());
+
+        }
+
+        class CheckBrackets
+        {
+            private string _str;
+            private readonly char[,] brackets = new char[,] { { '(', ')' }, { '[', ']' }, { '{', '}' } };
+            private Stack<char> bracketsQ = new Stack<char>();
+            public CheckBrackets(string str)
+            {
+                _str = str;
+            }
+
+            public bool validate()
+            {
+                foreach(char bracket in _str)
+                {
+                    for(int i = 0; i < brackets.GetLength(0); i++)
+                    {
+                        if (bracket == brackets[i, 1])
+                            if (bracketsQ.isEmpty() || bracketsQ.Pop() != brackets[i, 0])
+                                return false;
+                        if (bracket == brackets[i, 0])
+                            bracketsQ.Push(bracket);
+                    }
+                }
+
+                if (!bracketsQ.isEmpty())
+                    return false;
+
+                return true;
+            }
         }
 
         
