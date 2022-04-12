63eba7722381f91ca413497c2ade9a9ce06086a6
michal.mierzwa <michal.mierzwa@PK5-16>
2022-04-12 09:53:33 +0200

    added Node, Stack, Queue

--- Lab 7/Zad/Program.cs
+++ Lab 7/Zad/Program.cs
@@ -9,6 +9,55 @@ namespace Zad
             public T Value { get; set; }
             public Node<T> Next { get; set; }
         }
+
+        class Stack<T>
+        {
+            private Node<T> _head;
+            public void Push(T value)
+            {
+                Node<T> newNode = new Node<T> { Value = value, Next = _head };
+                _head = newNode;
+            }
+
+            public bool isEmpty() => _head == null;
+
+            public T Pop()
+            {
+                if (isEmpty())
+                    throw new Exception("stack is empty");
+                T result = _head.Value;
+                _head = _head.Next;
+                return result;
+            }
+        }
+
+        class Queue<T>
+        {
+            private Node<T> _head;
+            private Node<T> _tail;
+            public bool IsEmpty() => _head == null;
+            public void Insert(T value)
+            {
+                Node<T> node = new Node<T> { Value = value };
+                if(IsEmpty())
+                {
+                    _head = node;
+                    _tail = _head;
+                    return;
+                }
+                _tail.Next = node;
+                _tail = node;
+            }
+            public T Remove()
+            {
+                if (IsEmpty())
+                    throw new Exception("queue is empty");
+                T result = _head.Value;
+                _head = _head.Next;
+                return result;
+            }
+        }
+
         static void Main(string[] args)
         {
             Node<string> node = new Node<string> { Value = "adam" };
@@ -20,7 +69,23 @@ namespace Zad
                 Console.WriteLine(node.Value);
                 node = node.Next;
             }
-            
+
+            Stack<string> stack = new Stack<string>();
+            stack.Push("Adam");
+            stack.Push("Ewa");
+            stack.Push("Karol");
+            while (!stack.isEmpty())
+                Console.WriteLine(stack.Pop());
+
+            Queue<string> queue = new Queue<string>();
+            queue.Insert("Adam");
+            queue.Insert("Ewa");
+            queue.Insert("Karol");
+            while (!queue.IsEmpty())
+                Console.WriteLine(queue.Remove());
+
         }
+
+        
     }
 }
