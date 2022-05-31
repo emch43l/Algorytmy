27baed616d3d2afe0acbbde62689dbd9268897ea
michal.mierzwa <michal.mierzwa@PK5-16>
2022-05-31 10:29:46 +0200

    lab 10, 11, 12

---
+++ LAb 12/Zad/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Zad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int points = 0;
+            if(
+                LessonTasks.IsPalindrome("a")
+                && LessonTasks.IsPalindrome("aaa")
+                && LessonTasks.IsPalindrome("")
+                && LessonTasks.IsPalindrome("zakaz")
+                && LessonTasks.IsPalindrome("ZaKAZ")
+                && LessonTasks.IsPalindrome("KamilŚlimak")
+                && !LessonTasks.IsPalindrome("abc"))
+            {
+                Console.WriteLine("Zadadnie 1: OK");
+                points++;
+            }
+
+            if(
+                LessonTasks.IsAnagrams("abcd","bcda")
+                && LessonTasks.IsAnagrams("aa", "aa")
+                && !LessonTasks.IsAnagrams("AA", "aa")
+                && LessonTasks.IsAnagrams("","")
+                && !LessonTasks.IsAnagrams("abc", "abca")
+                )
+            {
+                Console.WriteLine("Zadadnie 2: OK");
+                points++;
+            }
+
+            if (
+                LessonTasks.LongestIdenticalString("aaaa").Equals("aaaa")
+                && LessonTasks.LongestIdenticalString("abcddddaaass").Equals("dddd")
+                && LessonTasks.LongestIdenticalString("abcd").Equals("a")
+                && LessonTasks.LongestIdenticalString("abbcdd").Equals("bb")
+                )
+            {
+                Console.WriteLine("Zadadnie 3: OK");
+                points+=2;
+            }
+
+            Console.WriteLine($"Liczba punktów: {points}");
+        }
+
+    }
+
+    class LessonTasks
+    {
+        public static bool IsPalindrome(string str)
+        {
+            str = str.ToLower();
+            char[] arr = str.ToCharArray();
+            Array.Reverse(arr);
+            return Enumerable.SequenceEqual(str.ToCharArray(), arr);
+        }
+
+        //czy łańcuchy są anargramami
+        public static bool IsAnagrams(string a, string b)
+        {
+            char[] arr1 = a.ToCharArray();
+            char[] arr2 = b.ToCharArray();
+            Array.Sort(arr1);
+            Array.Sort(arr2);
+            return Enumerable.SequenceEqual(arr2, arr1);
+        }
+
+        public static string LongestIdenticalString(string input)
+        {
+            char[] chars = input.ToCharArray();
+            List<char> result = new List<char>();
+            List<char> longest = new List<char>();
+            longest.Add(chars[0]);
+            int length = 1;
+            for(int i = 1; i < chars.Length; i++)
+            {
+                if(chars[i].Equals(longest[length - 1]))
+                {
+                    longest.Add(chars[i]);
+                    if (longest.Count() > result.Count())
+                    {
+                        result = new List<char>(longest);
+                    }
+                    length++;
+                }
+                else
+                {
+                    if(result.Count() == 0)
+                        result = new List<char>(longest);
+                    longest.Clear();
+                    longest.Add(chars[i]);
+                    length = 1;
+                }
+            }
+
+            return new String(result.ToArray());
+        }
+    }
+}
---
+++ Lab 11/Zad 1/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace Lab_11
+{
+    interface Igraph
+    {
+        bool AddDirectedEdge(int source, int target, int weight);
+        bool AddUnDirectedEdge(int source, int target, int weight);
+        void LevelTraversal(int source, Action<int> action);
+    }
+
+    class AdMatrix : Igraph
+    {
+        private int[,] _matrix;
+
+        public AdMatrix(int size)
+        {
+            _matrix = new int[size, size];
+        }
+
+        public bool AddDirectedEdge(int source, int target, int weight)
+        {
+            //kod sprawdzający source i target i  zwracający false dla niepoprawnych source i target
+            _matrix[source, target] = weight;
+            return true;
+        }
+
+        public bool AddUnDirectedEdge(int source, int target, int weight)
+        {
+            return AddDirectedEdge(source, target, weight) && AddDirectedEdge(target, source, weight);
+        }
+
+        public void LevelTraversal(int source, Action<int> action)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int row = 0; row < _matrix.GetLength(0); row++)
+            {
+                sb.Append($"wierzchołek {row} połączony z: ");
+                for (int col = 0; col < _matrix.GetLength(1); col++)
+                    if (_matrix[row, col] != 0)
+                        sb.Append($"{col} ");
+                sb.Append($"\n");
+            }
+
+            return sb.ToString();
+        }
+    }
+
+    record Edge
+    {
+        public int Node { get; set; }
+        public double Weight { get; set; }
+
+    }
+
+    class AdList : Igraph
+    {
+        Dictionary<int, HashSet<Edge>> _edges = new Dictionary<int, HashSet<Edge>>();
+        public bool AddDirectedEdge(int source, int target, int weight)
+        {
+            if (!_edges.ContainsKey(source))
+                _edges.Add(source, new HashSet<Edge>());
+            if (!_edges.ContainsKey(target))
+                _edges.Add(target, new HashSet<Edge>());
+            return _edges[source].Add(new Edge() { Node = target, Weight = weight });
+        }
+
+        public bool AddUnDirectedEdge(int source, int target, int weight)
+        {
+            return AddDirectedEdge(source, target, weight) && AddDirectedEdge(target, source, weight);
+        }
+
+        public void LevelTraversal(int source, Action<int> action)
+        {
+            Queue<int> q = new Queue<int>();
+            ISet<int> visited = new HashSet<int>();
+            q.Enqueue(source);
+            while(q.Count > 0)
+            {
+                int node = q.Dequeue();
+                if (visited.Contains(node))
+                    continue;
+                action.Invoke(node);
+                visited.Add(node);
+                HashSet<Edge> children = _edges[node];
+                foreach (Edge edge in children)
+                    q.Enqueue(edge.Node);
+            }
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Igraph graph = new AdList();
+            graph.AddDirectedEdge(0, 1, 1);
+            graph.AddDirectedEdge(1, 3, 1);
+            graph.AddDirectedEdge(2, 3, 1);
+            graph.AddDirectedEdge(3, 0, 1);
+            graph.AddDirectedEdge(0, 2, 1);
+            graph.AddDirectedEdge(3, 2, 1);
+            //Console.WriteLine(graph);
+            graph.LevelTraversal(1, x => Console.WriteLine(x));
+        }
+    }
+}
